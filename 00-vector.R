5cc2f1eb2e44003ac55fa8aee91bc18e10898ae8
nicyhmar <nicyhmar@gmail.com>
2023-03-23 14:51:33 +0530

    00-vector.R
    "
    
    l
    
    l
    git commit -m 00-vector.R

---
+++ 00-vector.R
@@ -0,0 +1,16 @@
+rm(list=ls() )
+# variable types
+# character, numeric and logical
+a = 1 # a is a numeric variable with 1 value
+b = 2
+a ==b
+a != b
+a > b
+a < b
+a >= b
+a <= b
+x = c(8, 3, 10) #use c() function for multiple inputs
+y = c(8, 1, 100)   
+# data types
+# vector
+# string split function
